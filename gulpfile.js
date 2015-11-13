6e873f973a0aed4f4178313f777dc77bd808a580
mikesankovich <misankovich@gmail.com>
2015-11-13 03:29:39 -0800

    shutdown

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-"use strict";
+// "use strict";
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
+
+var config = {
+  port: 9005,
+  debBaseUrl: 'http://localhost',
+  paths: {
+    
+  }
+};
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['dist'],
+    port: config.port,
+    base: config.devBaseUrl,
+    livereload: true
+  });
+});
